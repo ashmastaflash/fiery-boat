b101c0d38b99a0b3cf6e77be88ebeeb2de157770
Ash Wilson <ash.d.wilson@gmail.com>
2017-01-04 13:10:10 -0800

    new: usr: Adding Halo server and group reporting functionality

--- app/tasks.py
+++ app/tasks.py
@@ -1,27 +1,35 @@
+import apputils
 from celery import Celery
-import cloudpassage
-
 
 app = Celery(backend='redis://redis')
 
 
 @app.task
-def add(x, y):
-    return x + y
+def server_report(target):
+    """Accepts a hostname or server_id"""
+    halo = apputils.Halo()
+    return halo.generate_server_report(target)
 
 
 @app.task
-def mul(x, y):
-    return x * y
-
-
-@app.task
-def xsum(numbers):
-    return sum(numbers)
-
-
-@app.task
-def list_all_servers(halo_key, halo_secret):
-    halo_session = cloudpassage.HaloSession(halo_key, halo_secret)
+def list_all_servers():
+    creds = get_creds()
+    halo_session = cloudpassage.HaloSession(creds["halo_key"],
+                                            creds["halo_secret"])
     servers = cloudpassage.Server(halo_session)
+    return servers.list_all()
+
+
+@app.task
+def group_report(target):
+    halo = apputils.Halo()
+    returnhalo.generate_group_report(target)
+
+
+@app.task
+def list_all_groups():
+    creds = get_creds()
+    halo_session = cloudpassage.HaloSession(creds["halo_key"],
+                                            creds["halo_secret"])
+    servers = cloudpassage.ServerGroup(halo_session)
     return servers.list_all()
